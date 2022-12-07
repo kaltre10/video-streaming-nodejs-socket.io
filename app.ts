e12b519575f60a876b2694fea034b68980ff07a2
Jason Hernandez <kaltre10@gmail.com>
2022-12-06 22:14:03 -0500

    video streaming nodejs

---
+++ app.ts
@@ -0,0 +1,22 @@
+import express, {Request, Response} from 'express';
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from 'path';
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.get('/', (req:Request, res:Response) => 
+    res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.get('/dashboard', (req:Request, res:Response) => 
+    res.sendFile(path.join(__dirname, './public/dashboard.html')));
+
+io.on("connection", (socket) => {
+    socket.on("stream", (video) => {
+        socket.broadcast.emit("stream", video);
+    })
+});
+
+export = httpServer;
